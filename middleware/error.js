97e6c2aaf8603daf437f07b426ab44602ecc063d
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-07-05 10:50:06 -0400

    ErrorHanlder middleware

--- middleware/error.js
+++ middleware/error.js
@@ -28,7 +28,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || "server error",
+    error: error.message || " server error",
   });
 };
 
