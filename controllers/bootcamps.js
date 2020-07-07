433e35380c93ce5e0299df99c8f60b08d1d99a03
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-07-07 15:34:40 -0400

    Moved advanced results logic to its own middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,75 +8,7 @@ const geocoder = require("../utils/geocoder");
 //@route   GET /api/v1/bootcamps
 //@access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-  //Copy of req.query
-  const reqQuery = {...req.query};
-
-  //Field to exclude so they dont match as a field
-  const removeFields = ["select", "sort", "page", "limit"];
-
-  //Loop over removeFields and delete them from reqQuery
-  removeFields.forEach(param => delete reqQuery[param]);
-
-  //Create query string
-  let queryStr = JSON.stringify(reqQuery);
-
-  //Create operaters ($gt, $gte, etc)
-  queryStr = queryStr.replace(
-    /\b(gt|gte|lt|lte|in)\b()/g,
-    match => `$${match}`
-  );
-
-  //Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
-
-  //Select fields
-  if (req.query.select) {
-    const fields = req.query.select.split(",").join(" ");
-    query = query.select(fields);
-  }
-
-  //Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    //sory by descending createAt date
-    query = query.sort("-createdAt");
-  }
-
-  //Pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-  query = query.skip(startIndex).limit(limit);
-
-  //Exec query
-  const bootcamps = await query;
-
-  //Pagination result
-  const pagination = {};
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    pagination,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 //@desc    Get single bootcamp
@@ -84,7 +16,6 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 //@access  Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
-
   if (!bootcamp) {
     return next(
       new ErrorReponse(`Bootcamp not found with id of ${req.params.id}`, 404)
