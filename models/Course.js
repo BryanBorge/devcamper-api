2a7986e96d39d7dcc6a0c6fd5daa23aa91b2f068
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-07-07 14:03:00 -0400

    Added static method to aggregate average cost of courses for bootcamp

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 const geocoder = require("../utils/geocoder");
+const Bootcamp = require("./Bootcamp");
 
 const CourseSchema = new mongoose.Schema({
   title: {
@@ -38,6 +39,41 @@ const CourseSchema = new mongoose.Schema({
     ref: "Bootcamp",
     required: true,
   },
+});
+
+//Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log("Caclulating average cost".blue.inverse);
+
+  const obj = await this.aggregate([
+    {$match: {bootcamp: bootcampId}},
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: {
+          $avg: "$tuition",
+        },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//Call getAverageCost after save
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Call getAverageCost before remove
+CourseSchema.pre("remove", function () {
+  this.constructor.getAverageCost(this.bootcamp);
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
