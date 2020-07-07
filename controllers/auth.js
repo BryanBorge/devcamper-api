10fb82697318e0a5cd05c3a8a172c1fb6b2671dd
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-07-07 16:30:13 -0400

    Parse user data from body and send it to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,17 @@ const ErrorReponse = require("../utils/errorResponse");
 //@desc    Register user
 //@route   GET /api/v1/auth/register
 //@access  Public
-
 exports.register = asyncHandler(async (req, res, next) => {
+  const {name, email, password, role} = req.body;
+
+  console.log(name);
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({success: true});
 });
