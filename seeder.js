dcfd3306c768b513414b0150554db458727e6d2b
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-07-07 19:34:18 -0400

    Added user data to seeder

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({
 //Load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const Users = require("./models/User");
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI, {
@@ -27,12 +28,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 //Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await Users.create(users);
     console.log("Data imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -45,6 +50,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await Users.deleteMany();
     console.log("Data destroyed...".red.inverse);
     process.exit();
   } catch (err) {
