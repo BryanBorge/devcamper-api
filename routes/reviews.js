3abfa52ef6a6ec789cbddd40e1c70ba75312418a
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-07-08 13:13:26 -0400

    Added route for adding Review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {getReviews, getReview} = require("../controllers/reviews");
+const {getReviews, getReview, addReview} = require("../controllers/reviews");
 
 const Review = require("../models/Review");
 
 const advancedResults = require("../middleware/advancedResults");
+const {protect, authorize} = require("../middleware/auth");
 
 const router = express.Router({mergeParams: true});
 
@@ -12,7 +13,8 @@ router
   .get(
     advancedResults(Review, {path: "bootcamp", select: "name description"}),
     getReviews
-  );
+  )
+  .post(protect, authorize("user", "admin"), addReview);
 
 router.route("/:id").get(getReview);
 
